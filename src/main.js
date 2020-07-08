50d7d4fb3d6e5e27228389b25d39f384938323d2
杨腾祺 <3150475761@qq.com>
2020-07-09 00:05:31 +0800

    08-统一设置请求头

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:3000'
+axios.interceptors.request.use(config => {
+  if (!config.headers.Authorization && localStorage.getItem('token')) {
+    config.headers.Authorization = "Bearer " + localStorage.getItem('token')
+  }
+  return config
+})
 Vue.prototype.$axios = axios
 router.beforeEach((to, from, next) => {
   if (to.path !== '/login' && !localStorage.getItem('token')) {
